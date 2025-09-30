c2e244db6b479c7fab4c4d3443ec8fd7b076eef8
Darshan Pawar <darshanpawar010@gmail.com>
2025-10-01 00:56:23 +0530

    feat: return the sum of all numbers for comma and new line separated numbers input

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -3,8 +3,9 @@ class stringCalculator {
     if (numbers === "") {
       return 0;
     }
+    const separatorRegex = /,|\n/;
     const sum = numbers
-      ?.split(",")
+      ?.split(separatorRegex)
       ?.map((num) => parseInt(num))
       ?.reduce((sum, num) => sum + num, 0);
     return sum;
