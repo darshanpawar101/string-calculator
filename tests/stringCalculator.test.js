f29120a0a5f313e8f329eb2f0bf40f7fd5ff275c
Darshan Pawar <darshanpawar010@gmail.com>
2025-10-01 00:45:08 +0530

    test: add test to return sum of all numbers separated by commas

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -16,3 +16,7 @@ test("returns number for single input", () => {
 test("returns sum of two numbers separated by commas", () => {
   expect(calc.add("1,2")).toBe(3);
 });
+
+test("returns sum of all numbers separated by commas", () => {
+  expect(calc.add("1,2,3,4,5,6")).toBe(21);
+});
